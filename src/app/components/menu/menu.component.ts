ed4b7f6886d1d259519f1329a07641ad4d038f2b
Baraghen <samir.blom@gmail.com>
2020-02-14 06:02:17 +0100

    Basic google login functionality added. New structure for database. Some style changes have been made to all components.

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-menu',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   openNav() {
     if (window.screen.width <= 768) {
