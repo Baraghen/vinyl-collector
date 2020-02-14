8076801e9fea0206dbb48adb4361d514c2990169
Baraghen <samir.blom@gmail.com>
2020-02-14 16:40:59 +0100

    Added firebase to environment.prod.ts.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBVujGHUQhK9uWpH9_IEfaeCRkuqPhJEiQ",
+    authDomain: "vinyl-collector.firebaseapp.com",
+    databaseURL: "https://vinyl-collector.firebaseio.com",
+    projectId: "vinyl-collector",
+    storageBucket: "vinyl-collector.appspot.com",
+    messagingSenderId: "711439210305",
+    appId: "1:711439210305:web:e15166df3fa2bdb59d73c0",
+    measurementId: "G-PMDT9W8G9Y"
+  }
 };
